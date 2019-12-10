79e00c82f87b36a53618c8f7431852e72eb7dfd9
xiaoming <1406629722@qq.com>
2019-12-10 09:48:27 +0800

    Update formula

--- elaphant.rb
+++ elaphant.rb
@@ -1,9 +1,9 @@
 class Elaphant < Formula
   desc "elaphant node"
   homepage "https://github.com/elaphantapp/homebrew-elaphant"
-  url "https://github.com/elaphantapp/ElaphantNode/releases/download/v0.0.2/elaphant-0.0.2.tar.gz"
-  version "v0.0.2"
-  sha256 "bb72a3f14a42fbd94fb119212ca95313c0862387af7aec16a8be017f220937c0"
+  url "https://github.com/elaphantapp/ElaphantNode/releases/download/v0.1.0/elaphant-0.1.0-linux-amd64.tar.gz"
+  version "v0.1.0"
+  sha256 "3d950de031bbbe5607464d915cced40e19eeb04bf9d1dc5d397c82e6bc8bbc96"
 
   def install
     bin.install "elaphant"
